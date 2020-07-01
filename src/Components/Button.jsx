435d34fd8e7e16c818296a5be433743e340743f6
Noel Thomson <noel.thomson@gmail.com>
2020-07-01 15:25:48 -0400

    keypress functionality

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -9,13 +9,42 @@ const isOperator = (val) => {
   }
 };
 
-const Button = (props) => (
-  <div
-    className={`btn-wrapper ${isOperator(props.children) ? "operator" : null}`}
-    onClick={() => props.handleClick(props.children)}
-  >
-    {props.children}
-  </div>
-);
+class Button extends React.Component {
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeydown);
+    window.focus();
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeydown);
+  }
+
+  handleKeydown = (e) => {
+    if (e.key === this.props.children) {
+      this.props.handleClick(this.props.children);
+    }
+  };
+
+  render() {
+    return (
+      <div
+        className={`btn-wrapper ${
+          isOperator(this.props.children) ? "operator" : null
+        }`}
+        onClick={() => this.props.handleClick(this.props.children)}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+// const Button = (props) => (
+//   <div
+//     className={`btn-wrapper ${isOperator(props.children) ? "operator" : null}`}
+//     onClick={() => props.handleClick(props.children)}
+//   >
+//     {props.children}
+//   </div>
+// );
 
 export default Button;
